b6d8a3c011977b71e1f2b35a108bc43902ecc280
Anekdotin <eddwinn@gmail.com>
2022-12-01 08:53:40 -0500

    Added config file for wallet

---
+++ walletconfig.py
@@ -0,0 +1,12 @@
+from decimal import Decimal
+
+digital_currency = 1
+
+minconf = str(6)
+minamount = Decimal(0.00000001)
+maxamount = Decimal(0.5)
+
+rpcusername = 'rpcuser'
+rpcpassword = 'rpcpassword'
+
+url = "http://rpcuser:rpcpassword@127.0.0.1:8332/"
